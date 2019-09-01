3cf8c4ff8176e7c3d24e6747c09d10f25bcd737a
Sabin <sabin.bhandari15@gmail.com>
2019-09-02 00:27:34 +0200

    first commit

---
+++ server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '589527198781-8d98irjrvisqg30hnp9khdcc0c40tin3.apps.googleusercontent.com',
+    googleClientSecret: 'mT2gK942Ud1DmfLeFa6H0DGX',
+    mongoURI: 'mongodb+srv://p00gz:NtY4GJD4Xtmowb04@emaily-zvpuw.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'dsjfngkfbgoebgeroxvnodvnfvifbnfeoibeueeranzdjncjdnjfvjaaaerppklmz'
+};
